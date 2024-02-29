c50dab527e5899c6294b58d58682d9b22f8cb8bf
Lean Kurt <leankurtescano@gmail.com>
2024-02-29 08:06:28 +0800

    added calculator buttons through objects and made layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { calculatorButtons } from './constants';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='main-container'>
+      <input type='text' id='text' name='text'>
+      </input>
+      <div className='interface'>
+      {calculatorButtons.map((btn) => (
+        <div className='btn-container'>
+          <button key={btn.id}>{btn.value}</button>
+        </div>
+      ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
+
+ 
 
 export default App
