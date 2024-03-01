4f4c1f8ccec27a6ad027dab9311622fccc908466
Lean Kurt <leankurtescano@gmail.com>
2024-03-01 19:23:42 +0800

    added changes and some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import { calculatorButtons } from './constants';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState("");
   return (
     <div className='main-container'>
-      <input type='text' id='text' name='text'>
+      <input type='text' id='text' name='text' value={value}>
       </input>
       <div className='interface'>
-      {calculatorButtons.map((btn) => (
-        <div className='btn-container'>
-          <button key={btn.id}>{btn.value}</button>
-        </div>
-      ))}
+         <button className='btn'>AC</button>
+         <button className='btn' value="/" onClick={(e) => setValue(e.target.value)}>/</button>
+         <button className='btn' value="*" onClick={(e) => setValue(e.target.value)}>*</button>
+         <button className='btn' value="7" onClick={(e) => setValue(e.target.value)}>7</button>
+         <button className='btn' value="8" onClick={(e) => setValue(e.target.value)}>8</button>
+         <button className='btn' value="9" onClick={(e) => setValue(e.target.value)}>9</button>
+         <button className='btn' value="-" onClick={(e) => setValue(e.target.value)}>-</button>
+         <button className='btn' value="4" onClick={(e) => setValue(e.target.value)}>4</button>
+         <button className='btn' value="5" onClick={(e) => setValue(e.target.value)}>5</button>
+         <button className='btn' value="6" onClick={(e) => setValue(e.target.value)}>6</button>
+         <button className='btn' value="+" onClick={(e) => setValue(e.target.value)}>+</button>
+         <button className='btn' value="1" onClick={(e) => setValue(e.target.value)}>1</button>
+         <button className='btn' value="2" onClick={(e) => setValue(e.target.value)}>2</button>
+         <button className='btn' value="3" onClick={(e) => setValue(e.target.value)}>3</button>
+         <button className='btn' onClick={(e) => eval( e.value)}>=</button>
+         <button className='btn' value="."onClick={(e) => setValue(e.target.value)}>.</button>
       </div>
     </div>
   );
