859b9c4d18eaeeb11cd439aca3da4846c10a1dd1
Lean Kurt <leankurtescano@gmail.com>
2024-03-04 17:13:53 +0800

    added changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,57 @@ import { calculatorButtons } from './constants';
 import './App.css';
 
 function App() {
+  const handleClear = () => {
+    setValue('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      setValue(eval(input).toString());
+    } catch (error) {
+      setValue('Error');
+    }
+
+  }
+  const handleButtonClick = (value) => {
+    if (
+      (value === '+' || value === '-' || value === '*' || value === '/') &&
+      (input.slice(-1) === '+' ||
+        input.slice(-1) === '-' ||
+        input.slice(-1) === '*' ||
+        input.slice(-1) === '/')
+    ) {
+      // If the last character is an operator, don't allow adding another operator
+      return;
+    }
+
+    setValue(input + value);
+  };
+
+   
   const [value, setValue] = useState("");
   return (
     <div className='main-container'>
       <input type='text' id='text' name='text' value={value}>
       </input>
       <div className='interface'>
-         <button className='btn'>AC</button>
-         <button className='btn' value="/" onClick={(e) => setValue(e.target.value)}>/</button>
-         <button className='btn' value="*" onClick={(e) => setValue(e.target.value)}>*</button>
-         <button className='btn' value="7" onClick={(e) => setValue(e.target.value)}>7</button>
-         <button className='btn' value="8" onClick={(e) => setValue(e.target.value)}>8</button>
-         <button className='btn' value="9" onClick={(e) => setValue(e.target.value)}>9</button>
-         <button className='btn' value="-" onClick={(e) => setValue(e.target.value)}>-</button>
-         <button className='btn' value="4" onClick={(e) => setValue(e.target.value)}>4</button>
-         <button className='btn' value="5" onClick={(e) => setValue(e.target.value)}>5</button>
-         <button className='btn' value="6" onClick={(e) => setValue(e.target.value)}>6</button>
-         <button className='btn' value="+" onClick={(e) => setValue(e.target.value)}>+</button>
-         <button className='btn' value="1" onClick={(e) => setValue(e.target.value)}>1</button>
-         <button className='btn' value="2" onClick={(e) => setValue(e.target.value)}>2</button>
-         <button className='btn' value="3" onClick={(e) => setValue(e.target.value)}>3</button>
-         <button className='btn' onClick={(e) => eval( e.value)}>=</button>
-         <button className='btn' value="."onClick={(e) => setValue(e.target.value)}>.</button>
+      <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={handleClear}>AC</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
       </div>
     </div>
   );
